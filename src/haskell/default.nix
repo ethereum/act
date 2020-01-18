c7eb6ba2b2bc223fb5570862d630d61db16b96d1
Martin Lundfall <martin.lundfall@protonmail.com>
2020-01-18 17:46:11 +0100

    more wip splitter

--- src/haskell/default.nix
+++ src/haskell/default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, aeson, base, stdenv }:
+{ mkDerivation, aeson, array, base, bytestring, stdenv, text }:
 mkDerivation {
   pname = "act";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ aeson base ];
+  executableHaskellDepends = [ aeson array base bytestring text ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
