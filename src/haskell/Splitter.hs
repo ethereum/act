3d4e97e5fef86c95471080951fadc17efe1f15c0
Martin Lundfall <martin.lundfall@protonmail.com>
2020-01-22 12:51:26 +0100

    wip splitter

--- src/haskell/Splitter.hs
+++ src/haskell/Splitter.hs
@@ -18,39 +18,46 @@ import ErrM
 type StorageLayout = [TDecl]
 
 data Obligation = Obligation
-  { _name      :: Ident,
-    _contract  :: Ident,
+  { _name      :: String,
+    _contract  :: String,
     _StatusCode :: String,
-    _return     :: Exp,
+    _methodName :: String,
+    _inputArgs  :: [(String, String)]
+--    _return     :: Exp
 --    _env        :: [(String, Ident)],
-    _calldata  :: BYExp,
---    _variables :: [(Ident, Type)],
-    _preStore  :: [(Entry, Exp)],
-    _postStore :: [(Entry, Exp)],
-    _preCondition :: [BExp],
-    _postCondition :: [BExp]
+-- --    _variables :: [(Ident, Type)],
+--     _preStore  :: [(Entry, Exp)],
+--     _postStore :: [(Entry, Exp)],
+--     _preCondition :: [BExp],
+--     _postCondition :: [BExp]
   } deriving (Show)
 
 instance ToJSON Obligation where
-  toJSON (Obligation { .. }) = object [ "name" .= (show _name)
-           , "contract"  .= (show _contract)
-           , "statusCode"  .= (show _StatusCode)
-           , "return"  .= show _return
-           , "calldata"  .= show _calldata
-           , "preStore"  .= show _preStore
-           , "postStore"  .= show _postStore
-           , "preCondition"  .= show _preCondition
-           , "postCondition"  .= show _postCondition
+  toJSON (Obligation { .. }) =
+    object [ "name" .= _name
+           , "contract"  .= _contract
+           , "statusCode"  .= _StatusCode
+           , "methodName"  .= _methodName
+           , "inputArgs"   .= array _inputArgs
+--           , "return"  .= show _return
+           -- , "calldata"  .= show _calldata
+           -- , "preStore"  .= show _preStore
+           -- , "postStore"  .= show _postStore
+           -- , "preCondition"  .= show _preCondition
+           -- , "postCondition"  .= show _postCondition
            ]
 
 
-split :: Behaviour -> StorageLayout -> [Obligation]
-split (Transition name contract methodName args [] claim) storageVars =
+split :: Behaviour -> [Obligation]
+split (Transition (Ident name) (Ident contract) (Ident methodName) args [] claim) =
   case claim of
     Direct post -> [Obligation
       {_name     = name,
        _contract = contract,
-       _StatusCode = "EVMC_SUCCESS"
+       _StatusCode = "EVMC_SUCCESS",
+       _methodName = methodName,
+       _inputArgs = fmap (\(Dec ty (Ident id)) -> (show ty, show id)) args
+--       _return     = ""
 --       _return     = returnExp,
 --       _env        = defaultEnv,
 --       _calldata   = methodName args,
@@ -61,7 +68,7 @@ split (Transition name contract methodName args [] claim) storageVars =
        -- _postCondition = []
       }]
     CaseSplit _ -> error "TODO"
-split _ _ = error "The impossible has occurred"
+split _ = error "The impossible has occurred"
 
 usage :: IO ()
 usage = do
@@ -74,16 +81,13 @@ usage = do
 
 type ParseFun a = [Token] -> Err a
 
-run :: (Show a) => ParseFun a -> String -> IO (Act)
+run :: ParseFun Act -> String -> IO (Act)
 run p s = let ts = myLexer s in case p ts of
-           Bad s    -> do error "could not parse"
-           Ok  (Main b) -> do
-             putStrLn "\nParse Successful!"
-             return (Main b)
-           Ok _ -> error "not top level structure"
+           Bad s    -> error "could not parse"
+           Ok  (Main b) -> return (Main b)
 
-runFile :: (Show a) => ParseFun a -> FilePath -> IO ()
-runFile p f = putStrLn f >> readFile f >>= run p
+runFile :: ParseFun Act -> FilePath -> IO (Act)
+runFile p f = readFile f >>= run p
 
 main :: IO ()
 main = do
@@ -91,8 +95,8 @@ main = do
   case args of
     ["--help"] -> usage
     fs -> do
-      tree <- mapM_ (runFile pAct) fs
-      B.putStrLn $ encode tree
+      (Main b):[] <- mapM (runFile pAct) fs
+      mapM_ (B.putStrLn . encode . split ) b
     
 
 defaultEnv :: [(String, Ident)]
