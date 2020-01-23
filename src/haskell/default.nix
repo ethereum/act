d35cbfd58028fa523f49d1940cf78cce93186b77
Martin Lundfall <martin.lundfall@protonmail.com>
2020-01-23 20:23:18 +0100

    outlining krpint structure add iffH infra

--- src/haskell/default.nix
+++ src/haskell/default.nix
@@ -1,11 +1,15 @@
-{ mkDerivation, aeson, array, base, bytestring, stdenv, text }:
+{ mkDerivation, aeson, array, base, bytestring, stdenv, text
+, vector
+}:
 mkDerivation {
   pname = "act";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ aeson array base bytestring text ];
+  executableHaskellDepends = [
+    aeson array base bytestring text vector
+  ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
