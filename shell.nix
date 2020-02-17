ac0560397d1b55e7d0557cdbe005f06282450c47
Martin Lundfall <martin.lundfall@protonmail.com>
2020-02-17 17:52:51 +0100

    build process WIP

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {}; with haskellPackages;
 let
   drv = haskell.lib.addBuildTool (
-    haskellPackages.callPackage (import src/default.nix) {}
-  ) [cabal-install cabal2nix BNFC alex happy ghc];
+    callPackage (import ./src/default.nix) {}
+  ) [cabal-install cabal2nix BNFC];
 in
   drv
