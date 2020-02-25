6c3d41df7369fa218ca757f31a90ca8807aea65f
Martin Lundfall <martin.lundfall@protonmail.com>
2020-02-25 14:52:33 +0100

    allow for variables to start with _

--- tests/token/token.sol
+++ tests/token/token.sol
@@ -1,4 +1,4 @@
-// Copyright (C) 2017, 2018, 2019 dbrock, rain, mrchico
+// Copyright (C) 2020 Martin Lundfall
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
@@ -16,12 +16,6 @@
 pragma solidity >=0.4.24;
 
 contract Token {
-    // --- Auth ---
-    mapping (address => uint) public wards;
-    function rely(address guy)   public auth { wards[guy] = 1; }
-    function deny(address guy)   public auth { wards[guy] = 0; }
-    modifier auth { require(wards[msg.sender] == 1); _; }
-
     // --- ERC20 Data ---
     uint8   constant public decimals = 18;
     string  public name;
@@ -30,12 +24,10 @@ contract Token {
 
     mapping (address => uint)                      public balanceOf;
     mapping (address => mapping (address => uint)) public allowance;
-    mapping (address => uint)                      public nonces;
 
-    event Approval(address indexed src, address indexed guy, uint wad);
-    event Transfer(address indexed src, address indexed dst, uint wad);
+    event Approval(address indexed holder, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
 
-    // --- Math ---
     function add(uint x, uint y) internal pure returns (uint z) {
         require((z = x + y) >= x, "math-add-overflow");
     }
@@ -43,91 +35,32 @@ contract Token {
         require((z = x - y) <= x, "math-sub-underflow");
     }
 
-    // --- EIP712 niceties ---
-    bytes32 public DOMAIN_SEPARATOR;
-    bytes32 public permit_TYPEHASH = keccak256(
-        "Permit(address holder,address spender,uint256 nonce,uint256 deadline,bool allowed)"
-    );
-
     constructor(string memory symbol_, string memory name_, string memory version_, uint256 chainId_) public {
-        wards[msg.sender] = 1;
         symbol = symbol_;
-        name = name_;
-        DOMAIN_SEPARATOR = keccak256(abi.encode(
-            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-            keccak256(bytes("Dai Semi-Automated Permit Office")),
-            keccak256(bytes(version_)),
-            chainId_,
-            address(this)
-        ));
+        name   = name_;
+        totalSupply = _totalSupply;
+        balances[msg.sender] = _totalSupply;
     }
 
     // --- Token ---
-    function transfer(address dst, uint wad) public returns (bool) {
-        transferFrom(msg.sender, dst, wad);
+    function transfer(address to, uint value) public returns (bool) {
+        transferFrom(msg.sender, to, value);
         return true;
     }
-    function transferFrom(address src, address dst, uint wad)
+    function transferFrom(address from, address to, uint value)
         public returns (bool)
     {
-        if (src != msg.sender && allowance[src][msg.sender] != uint(-1)) {
-            allowance[src][msg.sender] = sub(allowance[src][msg.sender], wad);
+        if (from != msg.sender && allowance[from][msg.sender] != uint(-1)) {
+            allowance[from][msg.sender] = sub(allowance[from][msg.sender], value);
         }
-        balanceOf[src] = sub(balanceOf[src], wad);
-        balanceOf[dst] = add(balanceOf[dst], wad);
-        emit Transfer(src, dst, wad);
+        balanceOf[from] = sub(balanceOf[from], value);
+        balanceOf[to] = add(balanceOf[to], value);
+        emit Transfer(from, to, value);
         return true;
     }
-    function mint(address usr, uint wad) public auth {
-        balanceOf[usr] = add(balanceOf[usr], wad);
-        totalSupply    = add(totalSupply, wad);
-        emit Transfer(address(0), usr, wad);
-    }
-    function burn(address usr, uint wad) public {
-        if (usr != msg.sender && allowance[usr][msg.sender] != uint(-1)) {
-            allowance[usr][msg.sender] = sub(allowance[usr][msg.sender], wad);
-        }
-        balanceOf[usr] = sub(balanceOf[usr], wad);
-        totalSupply    = sub(totalSupply, wad);
-        emit Transfer(usr, address(0), wad);
-    }
-    function approve(address usr, uint wad) public returns (bool) {
-        allowance[msg.sender][usr] = wad;
-        emit Approval(msg.sender, usr, wad);
+    function approve(address spender, uint value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
         return true;
-    }
-
-    // --- Alias ---
-    function push(address usr, uint wad) public {
-        transferFrom(msg.sender, usr, wad);
-    }
-    function pull(address usr, uint wad) public {
-        transferFrom(usr, msg.sender, wad);
-    }
-    function move(address src, address dst, uint wad) public {
-        transferFrom(src, dst, wad);
-    }
-
-    // --- Approve by signature ---
-    function permit(address holder, address spender, uint256 nonce, uint256 deadline,
-                    bool allowed, uint8 v, bytes32 r, bytes32 s) public
-    {
-        bytes32 digest =
-            keccak256(abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR,
-                keccak256(abi.encode(permit_TYPEHASH,
-                                     holder,
-                                     spender,
-                                     nonce,
-                                     deadline,
-                                     allowed))
-        ));
-        require(holder == ecrecover(digest, v, r, s), "invalid permit");
-        require(deadline == 0 || deadline < now, "permit expied");
-        require(nonce == nonces[holder]++, "invalid nonce");
-        uint wad = allowed ? uint(-1) : 0;
-        allowance[holder][spender] = wad;
-        emit Approval(holder, spender, wad);
     }
 }
