b0f2d7ea5a59612846fe2bc8612bc875eb2281d9
Martin Lundfall <martin.lundfall@protonmail.com>
2020-04-15 18:59:36 +0200

    clean up default.nix

--- default.nix
+++ default.nix
@@ -10,33 +10,11 @@ let
       (import (dapptools + /overlay.nix))
     ];
   };
-  myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
-    overrides = self: super: rec {
-    sbv_8_4 = super.sbv_8_4.overrideAttrs (attrs: {
-      meta.broken = false;
-      postPatch = ''
-      sed -i -e 's|"z3"|"${nixpkgs.pkgs.z3}/bin/z3"|' Data/SBV/Provers/Z3.hs'';
-    });
-
-    hevm = nixpkgs.pkgs.haskell.lib.dontHaddock((self.callPackage (import (dapptools + /src/hevm)) {
-        # Haskell libs with the same names as C libs...
-        # Depend on the C libs, not the Haskell libs.
-        # These are system deps, not Cabal deps.
-        inherit (nixpkgs.pkgs) secp256k1;
-        ff = pkgs-for-dapp.libff;
-        }
-      ).overrideAttrs (attrs: {
-      postInstall = ''
-        wrapProgram $out/bin/hevm --suffix PATH \
-          : "${nixpkgs.lib.makeBinPath (with nixpkgs.pkgs; [bash coreutils git])}"
-      '';
-
-      enableSeparateDataOutput = true;
-      buildInputs = attrs.buildInputs ++ [nixpkgs.pkgs.solc nixpkgs.pkgs.z3];
-      nativeBuildInputs = attrs.nativeBuildInputs ++ [nixpkgs.pkgs.makeWrapper];
-      }));
-};
-};
+  haskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override (old: {
+    overrides = nixpkgs.pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (
+      import (dapptools + /haskell.nix) { lib = nixpkgs.pkgs.lib; pkgs = pkgs-for-dapp; }
+    );
+  });
 in
 
-myHaskellPackages.callPackage (import ./src/default.nix) {}
+haskellPackages.callPackage (import ./src/default.nix) {}
