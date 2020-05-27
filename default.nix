37ff817da641a45028a7eb3bdb4b73220a21d7c1
Martin Lundfall <martin.lundfall@protonmail.com>
2020-05-27 17:47:36 +0200

    pin nixpkgs to 19.09

--- default.nix
+++ default.nix
@@ -1,5 +1,11 @@
-{nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865"}:
+{compiler ? "ghc865"}:
 let
+  nixpkgs = import (builtins.fetchGit {
+    url = "https://github.com/nixos/nixpkgs";
+    ref = "release-19.09";
+    rev = "54e89941c303687a6392a3264dbe1540fe3381d8";
+  }) {};
+#  nixpkgs = import <nixpkgs> {};
   dapptools = builtins.fetchGit {
     url = "https://github.com/dapphub/dapptools.git";
     rev = "509a5741f03ea4d80a4901e1f6a6a9ac4b443ec0";
