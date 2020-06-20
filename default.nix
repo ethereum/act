1d312158b64e0b99d08b8a027c4eddd0ee4f1252
David Terry <me@xwvvvvwx.com>
2020-06-20 18:42:02 +0200

    nix: rework infra
    - pin nixpkgs
    - share code between shell.nix and default.nix
    - automatically call cabal2nix

--- default.nix
+++ default.nix
@@ -1,20 +1,47 @@
-{nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865"}:
-let
-  dapptools = builtins.fetchGit {
-    url = "https://github.com/dapphub/dapptools.git";
-    rev = "c44db1f252b870c0913d0f83093347e9c05469c9";
-    ref = "symbolic";
-  };
-  pkgs-for-dapp = import <nixpkgs> {
-    overlays = [
-      (import (dapptools + /overlay.nix))
-    ];
-  };
-  haskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override (old: {
-    overrides = nixpkgs.pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (
-      import (dapptools + /haskell.nix) { lib = nixpkgs.pkgs.lib; pkgs = pkgs-for-dapp; }
-    );
-  });
-in
+{ compiler ? "ghc865" }:
 
-haskellPackages.callPackage (import ./src/default.nix) {}
+let
+  # git ls-remote https://github.com/NixOS/nixpkgs-channels.git nixos-19.09
+  nixpkgs-commit = "289466dd6a11c65a7de4a954d6ebf66c1ad07652";
+  pkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/tarball/${nixpkgs-commit}";
+    sha256 = "0r5ja052s86fr54fm1zlhld3fwawz2w1d1gd6vbvpjrpjfyajibn";
+  }) {};
+
+  dapptools = import (fetchGit {
+    url = https://github.com/dapphub/dapptools.git;
+    ref = "symbolic";
+  }) {};
+
+  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hself: hsuper: {
+      hevm = dapptools.haskellPackages.hevm;
+      sbv = dapptools.haskellPackages.sbv_8_6;
+      act =
+        hself.callCabal2nix
+          "act"
+          (gitignore ./src)
+          {};
+    };
+  };
+
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p.act
+    ];
+    buildInputs = with pkgs.haskellPackages; [
+      myHaskellPackages.cabal-install
+    ];
+    withHoogle = true;
+  };
+
+  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages.act);
+in
+{
+  inherit shell;
+  inherit exe;
+  inherit myHaskellPackages;
+  act = myHaskellPackages.act;
+}
