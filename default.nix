fceb3ba6749fd2439b25cfacd5cfb82b189b768b
David Terry <me@xwvvvvwx.com>
2020-08-04 14:19:48 +0200

    nix: full reproducibility
    - manage deps with niv
    - bump to lastest dapptools
    - bump to nixos-20.03

--- default.nix
+++ default.nix
@@ -1,24 +1,16 @@
 { compiler ? "ghc865" }:
 
 let
-  # git ls-remote https://github.com/NixOS/nixpkgs-channels.git nixos-19.09
-  nixpkgs-commit = "289466dd6a11c65a7de4a954d6ebf66c1ad07652";
-  pkgs = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs-channels/tarball/${nixpkgs-commit}";
-    sha256 = "0r5ja052s86fr54fm1zlhld3fwawz2w1d1gd6vbvpjrpjfyajibn";
-  }) {};
-
-  dapptools = import (fetchGit {
-    url = https://github.com/dapphub/dapptools.git;
-    ref = "symbolic";
-  }) {};
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+  dapptools = import sources.dapptools {};
 
   gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
 
   myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = hself: hsuper: {
       hevm = dapptools.haskellPackages.hevm;
-      sbv = dapptools.haskellPackages.sbv_8_6;
+      sbv = dapptools.haskellPackages.sbv;
       act =
         hself.callCabal2nix
           "act"
@@ -30,6 +22,7 @@ let
   shell = myHaskellPackages.shellFor {
     packages = p: [
       p.act
+      p.niv
     ];
     buildInputs = with pkgs.haskellPackages; [
       myHaskellPackages.cabal-install
