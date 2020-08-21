79159b5c432c8d7686d0a398ade22fdde6f42a48
Martin Lundfall <martin.lundfall@protonmail.com>
2020-08-21 16:38:04 +0200

    update parse test vectors

--- tests/smoke/smoke.act.parsed.hs
+++ tests/smoke/smoke.act.parsed.hs
@@ -1,1 +1,1 @@
-[Transition "f" "A" f(uint256 x) [] (Branch (AlexPn 0 0 0) (BoolLit True) [Leaf (AlexPn 0 0 0) (BoolLit True) (Post Nothing [] (Just (IntLit 1)))]) Nothing]
+[Transition "f" "A" f(uint256 x) [] (Direct (Post Nothing [] (Just (IntLit 1)))) Nothing]
