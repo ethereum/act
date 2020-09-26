1197b37e8347d9e4e8c6de24cb0a05f9f0a94f76
endorphin <endorphin@users.noreply.github.com>
2020-09-25 21:15:31 -0400

    + library for user coq developments + do not generate indentation

---
+++ lib/ActLib.v
@@ -0,0 +1,13 @@
+(** library to be included with user coq developments *)
+
+Require Import Coq.ZArith.ZArith.
+Open Scope Z_scope.
+
+(** addresses are interpreted as integers *)
+Definition address := Z.
+
+(** integer bounds *)
+Definition UINT_MIN (n : Z) := 0.
+Definition UINT_MAX (n : Z) := 2^n - 1.
+Definition INT_MIN  (n : Z) := 0 - 2^(n - 1).
+Definition INT_MAX  (n : Z) := 2^(n - 1) - 1.
