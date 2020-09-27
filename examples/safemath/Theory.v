b81d14f130551452ae02602e08f3a31c4197c67f
endorphin <endorphin@users.noreply.github.com>
2020-09-26 22:53:16 -0400

    coq example for safemath

---
+++ examples/safemath/Theory.v
@@ -0,0 +1,97 @@
+Require Import SafeMath.SafeMath.
+Require Import ActLib.ActLib.
+Require Import Coq.ZArith.ZArith.
+Open Scope Z_scope.
+
+(* trivial observation that there is only one possible state *)
+Lemma state_constant : forall s, s = state.
+Proof.
+  intros.
+  destruct s.
+  reflexivity.
+Qed.
+
+Theorem mul_correct : forall s y x, reachable s ->
+  range256 x /\ range256 y /\ range256 (x * y) <-> mul_ret s y x = Some (x * y).
+
+Proof.
+  intros.
+  split. {
+
+    intros.
+    destruct H0 as [Hx [Hy Hxy]].
+    rewrite (state_constant s).
+    unfold mul_ret. simpl.
+
+    assert (0 <=? x * y = true) as assertion. {
+      apply (proj2 (Z.leb_le 0 (x * y))).
+      destruct Hxy. assumption.
+    }
+    rewrite assertion. clear assertion.
+
+    assert (x * y <=? UINT_MAX 256 = true) as assertion. {
+      apply (proj2 (Z.leb_le (x * y) (UINT_MAX 256))).
+      destruct Hxy. assumption.
+    }
+    rewrite assertion. clear assertion.
+
+    assert (0 <=? y = true) as assertion. {
+      apply (proj2 (Z.leb_le 0 y)).
+      destruct Hy. assumption.
+    }
+    rewrite assertion. clear assertion.
+
+    assert (y <=? UINT_MAX 256 = true) as assertion. {
+      apply (proj2 (Z.leb_le y (UINT_MAX 256))).
+      destruct Hy. assumption.
+    }
+    rewrite assertion. clear assertion.
+
+    assert (0 <=? x = true) as assertion. {
+      apply (proj2 (Z.leb_le 0 x)).
+      destruct Hx. assumption.
+    }
+    rewrite assertion. clear assertion.
+
+    assert (x <=? UINT_MAX 256 = true) as assertion. {
+      apply (proj2 (Z.leb_le x (UINT_MAX 256))).
+      destruct Hx. assumption.
+    }
+    rewrite assertion. clear assertion.
+
+    simpl. reflexivity.
+
+  } {
+
+    intros.
+    unfold mul_ret in H0.
+    simpl in H0.
+    apply (ite_true _ (Some (x * y)) None) in H0.
+    apply andb_prop in H0.
+    destruct H0.
+    apply andb_prop in H0.
+    destruct H0.
+    apply andb_prop in H1.
+    destruct H1.
+    apply andb_prop in H1.
+    destruct H1.
+    apply andb_prop in H3.
+    destruct H3.
+    apply andb_prop in H3.
+    destruct H3.
+    clear H5.
+    unfold range256.
+    split. split.
+    apply Zle_bool_imp_le. assumption.
+    apply Zle_bool_imp_le. assumption.
+    split. split.
+    apply Zle_bool_imp_le. assumption.
+    apply Zle_bool_imp_le. assumption.
+    split.
+    apply Zle_bool_imp_le. assumption.
+    apply Zle_bool_imp_le. assumption.
+    unfold not. intros. discriminate.
+
+  }
+Qed.
+
--- lib/ActLib.v
+++ lib/ActLib.v
@@ -3,14 +3,38 @@
 Require Import Coq.ZArith.ZArith.
 Open Scope Z_scope.
 
-(** addresses are interpreted as integers *)
+(** * type definitions *)
 Definition address := Z.
 
-(** integer bounds *)
+(** * integer bounds *)
 Definition UINT_MIN (n : Z) := 0.
 Definition UINT_MAX (n : Z) := 2^n - 1.
 Definition INT_MIN  (n : Z) := 0 - 2^(n - 1).
 Definition INT_MAX  (n : Z) := 2^(n - 1) - 1.
 
-(** notations *)
+(** * notations *)
 Notation "a =?? b" := (bool_eq a b) (at level 70, no associativity).
+Definition range256 (n : Z) := 0 <= n <= UINT_MAX 256.
+
+(** * lemmas *)
+
+Lemma ite_true {A : Type} : forall (b : bool) (x y : A),
+  ((if b then x else y) = x) -> (x <> y) -> (b = true).
+Proof.
+  intros b x y H Hneq.
+  induction b.
+  - reflexivity.
+  - apply eq_sym in H.
+    destruct (Hneq H).
+Qed.
+
+Lemma ite_false {A : Type} : forall (b : bool) (x y : A),
+  ((if b then x else y) = y) -> (x <> y) -> (b = false).
+Proof.
+  intros b x y H Hneq.
+  induction b.
+  - unfold not in Hneq. destruct (Hneq H).
+  - reflexivity.
+Qed.
+
+
