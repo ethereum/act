7357f427565cc798cd61f045d4cb8c73a57bbf74
David Terry <me@xwvvvvwx.com>
2021-01-26 13:57:20 +0100

    nix: switch to haskell.nix
    The parameterized-utils package was broken in nixpkgs, so switching to
    the IOHK nix/haskell infrastructure since this allows us to build any
    version of any package that appears in hackage.

--- shell.nix
+++ shell.nix
@@ -1,1 +1,14 @@
-(import ./default.nix {}).shell
+let
+  pkgs = import ./default.nix;
+in
+  pkgs.shellFor {
+    packages = ps: [ ps.act ];
+    tools = {
+      haskell-language-server = "latest";
+      cabal-install = "latest";
+      happy = "latest";
+      alex = "latest";
+    };
+    withHoogle = true;
+  }
+
