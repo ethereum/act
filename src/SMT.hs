b22100c857f94af83909d816794e7d6798db34f9
David Terry <me@xwvvvvwx.com>
2021-04-28 11:58:00 +0200

    SMT: formatting

--- src/SMT.hs
+++ src/SMT.hs
@@ -34,16 +34,16 @@ data SMTConfig = SMTConfig
 
 type SMT2 = String
 
--- Some expressions should be constructed over the poststate, and some over the prestate...
+-- | Some expressions should be constructed over the poststate, and some over the prestate...
 data When = Pre | Post
 
 instance Show When where
   show Pre = "pre"
   show Post = "post"
 
--- An SMTExp is a structured representation of an SMT Expression
--- The _storage, _calldata, and _environment fields hold variable declarations
--- The assertions field holds the various constraints that should be satisfied
+-- | An SMTExp is a structured representation of an SMT Expression
+--   The _storage, _calldata, and _environment fields hold variable declarations
+--   The assertions field holds the various constraints that should be satisfied
 data SMTExp = SMTExp
   { _storage :: [SMT2]
   , _calldata :: [SMT2]
@@ -90,10 +90,12 @@ data Model = Model
 mkPostconditionQueries :: Claim -> [Query]
 mkPostconditionQueries (B behv@(Behaviour _ _ _ (Interface _ decls) preconds postconds stateUpdates _)) = mkQuery <$> postconds
   where
+    -- declare vars
     storage = concatMap (declareStorageLocation . getLoc) stateUpdates
     args = declareArg <$> decls
     envs = declareEthEnv <$> ethEnvFromBehaviour behv
 
+    -- constraints
     pres = mkAssert Pre <$> preconds
     updates = encodeUpdate <$> stateUpdates
 
@@ -106,11 +108,13 @@ mkPostconditionQueries (B behv@(Behaviour _ _ _ (Interface _ decls) preconds pos
     mkQuery e = Postcondition (B behv) e (mksmt e)
 mkPostconditionQueries (C constructor@(Constructor _ _ (Interface _ decls) preconds postconds initialStorage stateUpdates)) = mkQuery <$> postconds
   where
+    -- declare vars
     localStorage = declareInitialStorage <$> initialStorage
     externalStorage = concatMap (declareStorageLocation . getLoc) stateUpdates
     args = declareArg <$> decls
     envs = declareEthEnv <$> ethEnvFromConstructor constructor
 
+    -- constraints
     pres = mkAssert Pre <$> preconds
     updates = encodeUpdate <$> stateUpdates
     initialStorage' = encodeInitialStorage <$> initialStorage
@@ -141,29 +145,29 @@ mkPostconditionQueries _ = []
 --     - Asserts that internal storage values have the value given in the creates block
 --     - Asserts that the invariant does not hold over the poststate
 --
---  If all of the queries return `unsat` then we have an inductive proof that the invariant holds for all possible contract states.
+--   If all of the queries return `unsat` then we have an inductive proof that
+--   the invariant holds for all possible contract states.
 mkInvariantQueries :: [Claim] -> [Query]
-mkInvariantQueries claims = concatMap mkQuery gathered
+mkInvariantQueries claims = concatMap mkQueries gathered
   where
-    gathered = fmap (\inv -> (inv, definition inv, getBehaviours inv)) invariants
-    invariants = [i | I i <- claims]
-    getBehaviours (Invariant c _ _ _) = filter (\b -> isPass b && contractMatches c b) [b | B b <- claims]
-    definition (Invariant c _ _ _) = head $ filter
-      (\b -> Pass == _cmode b && _cname b == c)
-      [c' | C c' <- claims]
-    contractMatches c b = c == (_contract b)
-    isPass b = (_mode b) == Pass
+    mkQueries (inv, constructor, behvs) = mkInit inv constructor : fmap (mkBehv inv constructor) behvs
+    gathered = fmap (\inv -> (inv, getConstructor inv, getBehaviours inv)) [i | I i <- claims]
 
-    mkQuery (inv, constructor, behvs) = mkInit inv constructor : fmap (mkBehv inv constructor) behvs
+    getBehaviours (Invariant c _ _ _) = filter (matchBehaviour c) [b | B b <- claims]
+    getConstructor (Invariant c _ _ _) = head $ filter (matchConstructor c) [c' | C c' <- claims]
+    matchBehaviour contract behv = (_mode behv) == Pass && (_contract behv) == contract
+    matchConstructor contract defn = _cmode defn == Pass && _cname defn == contract
 
     mkInit :: Invariant -> Constructor -> Query
     mkInit inv@(Invariant _ invConds _ invExp) constr@(Constructor _ _ (Interface _ decls) preconds _ initialStorage stateUpdates) = Inv (C constr) inv smt
       where
+        -- declare vars
         localStorage = declareInitialStorage <$> initialStorage
         externalStorage = concatMap (declareStorageLocation . getLoc) stateUpdates
         args = declareArg <$> decls
         envs = declareEthEnv <$> (ethEnvFromConstructor constr)
 
+        -- constraints
         pres = (mkAssert Pre <$> preconds) <> (mkAssert Pre <$> invConds)
         updates = encodeUpdate <$> stateUpdates
         initialStorage' = encodeInitialStorage <$> initialStorage
@@ -181,23 +185,24 @@ mkInvariantQueries claims = concatMap mkQuery gathered
         (Interface _ initDecls) = _cinterface constr
         (Interface _ behvDecls) = _interface behv
 
-        -- storage locs mentioned in the invariant but not in the behaviour
-        implicitStorageLocs = locsFromExp invExp \\ (getLoc <$> _stateUpdates behv)
-
-        storage = concatMap (declareStorageLocation . getLoc) (_stateUpdates behv) <> (concatMap declareStorageLocation implicitStorageLocs)
+        -- declare vars
+        invLocs = concatMap declareStorageLocation (locsFromExp invExp)
+        behvLocs = concatMap (declareStorageLocation . getLoc) (_stateUpdates behv)
         initArgs = declareArg <$> initDecls
         behvArgs = declareArg <$> behvDecls
-        envs = declareEthEnv <$> (ethEnvFromBehaviour behv <> ethEnvFromConstructor constr)
+        invEnv = declareEthEnv <$> (ethEnvFromExp invExp)
+        behvEnv = declareEthEnv <$> (ethEnvFromBehaviour behv)
 
+        -- constraints
         preInv = mkAssert Pre invExp
         postInv = mkAssert Post . Neg $ invExp
         preConds = mkAssert Pre <$> (_preconditions behv <> invConds <> invStorageBounds)
-        updates = encodeUpdate <$> (_stateUpdates behv <> (Left <$> implicitStorageLocs))
+        updates = encodeUpdate <$> (_stateUpdates behv <> (Left <$> (locsFromExp invExp)))
 
         smt = SMTExp
-          { _storage = storage
+          { _storage = invLocs <> behvLocs
           , _calldata = initArgs <> behvArgs
-          , _environment = envs
+          , _environment = invEnv <> behvEnv
           , _assertions = preConds <> updates <> [preInv, postInv]
           }
 
@@ -208,38 +213,35 @@ runQuery conf q = do
       containsErrors = any (isPrefixOf "(error") output
       res = case exitCode of
         ExitFailure code -> Error code stdout
-        ExitSuccess -> if containsErrors
-                          then Error 0 stdout -- cvc4 returns exit code zero even if there are smt errors present... :/
-                          else case output of
-                                 [] -> Unknown
-                                 l -> case last l of
-                                   "sat" -> Sat
-                                   "unsat" -> Unsat
-                                   "timeout" -> Unknown -- TODO: disambiguate
-                                   "unknown" -> Unknown
-                                   _ -> Error 0 $ "Unable to parse SMT output: " <> stdout
+        ExitSuccess ->
+          if containsErrors
+          then Error 0 stdout -- cvc4 returns exit code zero even if there are errors
+          else case output of
+                 [] -> Unknown
+                 l -> case last l of
+                   "sat" -> Sat
+                   "unsat" -> Unsat
+                   "timeout" -> Unknown -- TODO: disambiguate
+                   "unknown" -> Unknown
+                   _ -> Error 0 $ "Unable to parse SMT output: " <> stdout
   pure (q, res)
 
 runSMT :: SMTConfig -> SMT2 -> IO (ExitCode, String, String)
 runSMT (SMTConfig solver timeout _) e = do
   let input = intercalate "\n" ["(set-logic ALL)", e]
       args = case solver of
-               Z3 -> ["-in", "-t:" <> show timeout]
-               CVC4 -> ["--lang=smt", "--interactive", "--no-interactive-prompt", "--tlimit=" <> show timeout]
+               Z3 ->
+                 [ "-interactive"
+                 , "-t:" <> show timeout]
+               CVC4 ->
+                 [ "--lang=smt"
+                 , "--interactive"
+                 , "--no-interactive-prompt"
+                 , "--tlimit=" <> show timeout]
   readProcessWithExitCode (show solver) args input
 
 
 --- SMT2 generation ---
-
--- | encodes a storge update rewrite as an smt assertion
-encodeUpdate :: Either StorageLocation StorageUpdate -> SMT2
-encodeUpdate (Left loc) = "(assert (= " <> nameFromLoc Pre loc <> " " <> nameFromLoc Post loc <> "))"
-encodeUpdate (Right update) = case update of
-  IntUpdate item e -> encode item e
-  BoolUpdate item e -> encode item e
-  BytesUpdate item e -> encode item e
-  where
-    encode item e = "(assert (= " <> expToSMT2 Post (TEntry item) <> " " <> expToSMT2 Pre e <> "))"
 
 -- | encodes a storage update from a constructor creates block as an smt assertion
 encodeInitialStorage :: StorageUpdate -> SMT2
@@ -250,14 +252,8 @@ encodeInitialStorage update = case update of
   where
     encode item e = "(assert (= " <> expToSMT2 Post (TEntry item) <> " " <> expToSMT2 Post e <> "))"
 
-declareArg :: Decl -> SMT2
-declareArg (Decl typ name) = constant name (metaType typ)
-
-declareEthEnv :: EthEnv -> SMT2
-declareEthEnv env = constant (prettyEnv env) tp
-  where tp = fromJust . lookup env $ defaultStore
-
--- | declares a storage location that is created by the constructor, these locations have no prestate, so we declare a post var only
+-- | declares a storage location that is created by the constructor, these
+--   locations have no prestate, so we declare a post var only
 declareInitialStorage :: StorageUpdate -> SMT2
 declareInitialStorage update = case getLoc . Right $ update of
   IntLoc item -> case item of
@@ -273,7 +269,18 @@ declareInitialStorage update = case getLoc . Right $ update of
     mkdirect item tp = constant (nameFromItem Post item) tp
     mkarray item ixs tp = array (nameFromItem Post item) ixs tp
 
--- declares a storage location that exists both in the pre state and the post state (i.e. anything except a loc created by a constructor claim)
+-- | encodes a storge update rewrite as an smt assertion
+encodeUpdate :: Either StorageLocation StorageUpdate -> SMT2
+encodeUpdate (Left loc) = "(assert (= " <> nameFromLoc Pre loc <> " " <> nameFromLoc Post loc <> "))"
+encodeUpdate (Right update) = case update of
+  IntUpdate item e -> encode item e
+  BoolUpdate item e -> encode item e
+  BytesUpdate item e -> encode item e
+  where
+    encode item e = "(assert (= " <> expToSMT2 Post (TEntry item) <> " " <> expToSMT2 Pre e <> "))"
+
+-- | declares a storage location that exists both in the pre state and the post
+--   state (i.e. anything except a loc created by a constructor claim)
 declareStorageLocation :: StorageLocation -> [SMT2]
 declareStorageLocation loc = case loc of
   IntLoc item -> case item of
@@ -288,6 +295,13 @@ declareStorageLocation loc = case loc of
   where
     mkdirect item tp = [constant (nameFromItem Pre item) tp, constant (nameFromItem Post item) tp]
     mkarray item ixs tp = [array (nameFromItem Pre item) ixs tp, array (nameFromItem Post item) ixs tp]
+
+declareArg :: Decl -> SMT2
+declareArg (Decl typ name) = constant name (metaType typ)
+
+declareEthEnv :: EthEnv -> SMT2
+declareEthEnv env = constant (prettyEnv env) tp
+  where tp = fromJust . lookup env $ defaultStore
 
 returnExpToSMT2 :: When -> ReturnExp -> SMT2
 returnExpToSMT2 w e = case e of
@@ -317,7 +331,9 @@ expToSMT2 w e = case e of
   Div a b -> binop "div" a b
   Mod a b -> binop "mod" a b
   Exp a b -> expToSMT2 w (simplifyExponentiation a b)
-  LitInt a -> if a > 0 then show a else "(- " <> (show . negate $ a) <> ")" -- cvc4 does not accept negative integer literals...
+  LitInt a -> if a > 0
+              then show a
+              else "(- " <> (show . negate $ a) <> ")" -- cvc4 does not accept negative integer literals
   IntVar a -> a
   IntEnv a -> prettyEnv a
 
@@ -351,21 +367,25 @@ expToSMT2 w e = case e of
     MappedBytes _ _ ixs -> select (nameFromItem w item) ixs
 
   where
+    asSMT2 :: Exp a -> SMT2
+    asSMT2 = expToSMT2 w
+
     unop :: String -> Exp a -> SMT2
-    unop op a = "(" <> op <> " " <> expToSMT2 w a <> ")"
+    unop op a = "(" <> op <> " " <> asSMT2 a <> ")"
 
     binop :: String -> Exp a -> Exp b -> SMT2
-    binop op a b = "(" <> op <> " " <> expToSMT2 w a <> " " <> expToSMT2 w b <> ")"
+    binop op a b = "(" <> op <> " " <> asSMT2 a <> " " <> asSMT2 b <> ")"
 
     triop :: String -> Exp a -> Exp b -> Exp c -> SMT2
-    triop op a b c = "(" <> op <> " " <> expToSMT2 w a <> " " <> expToSMT2 w b <> " " <> expToSMT2 w c <> ")"
+    triop op a b c = "(" <> op <> " " <> asSMT2 a <> " " <> asSMT2 b <> " " <> asSMT2 c <> ")"
 
     select :: String -> NonEmpty ReturnExp -> SMT2
     select name (hd :| tl) = foldl' (\smt ix -> "(select " <> smt <> " " <> returnExpToSMT2 w ix <> ")") inner tl
       where
         inner = "(" <> "select" <> " " <> name <> " " <> returnExpToSMT2 w hd <> ")"
 
--- | SMT2 has no support for exponentiation, but we can do some preprocessing if the RHS is concrete to provide some limited support for exponentiation
+-- | SMT2 has no support for exponentiation, but we can do some preprocessing
+--   if the RHS is concrete to provide some limited support for exponentiation
 --   TODO: support any exponentiation expression where the RHS evaluates to a concrete value
 simplifyExponentiation :: Exp Integer -> Exp Integer -> Exp Integer
 simplifyExponentiation (LitInt a) (LitInt b) = (LitInt (a ^ b))
