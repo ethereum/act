ec7acd7b918c9101944a2f948df734586d66fad4
Jack Ek <2727556+kjekac@users.noreply.github.com>
2021-04-30 16:41:33 +0200

    update tests

--- tests/frontend/pass/smoke/smoke.act.parsed.hs
+++ tests/frontend/pass/smoke/smoke.act.parsed.hs
@@ -1,1 +1,1 @@
-[Transition "f" "A" f(uint256 x) [] (Direct (Post Nothing [] (Just (IntLit (AlexPn 46 4 9) 1)))) Nothing]
+[Transition "f" "A" f(uint256 x) [] (Direct (Post Nothing [] (Just (IntLit (AlexPn 46 4 9) 1)))) []]
