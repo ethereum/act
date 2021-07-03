e340b5fa3e59a87beca7940f7ea568973761f843
David Terry <me@xwvvvvwx.com>
2021-07-03 20:46:05 +0200

    bump dapptools & nix

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ compiler ? "ghc884" }:
+{ compiler ? "ghc8104" }:
 
 let
   sources = import ./nix/sources.nix;
@@ -7,12 +7,12 @@ let
 
   gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
 
-  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = hself: hsuper: {
-      hevm = dapptools.haskellPackages.hevm;
-      sbv = dapptools.haskellPackages.sbv;
+  haskell = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: {
+      hevm = dapptools.sharedHaskellPackages.hevm;
+      sbv = dapptools.sharedHaskellPackages.sbv;
       act =
-        pkgs.haskell.lib.dontCheck (hself.callCabal2nixWithOptions
+        pkgs.haskell.lib.dontCheck (self.callCabal2nixWithOptions
           "act"
           (gitignore ./src)
           "-fci"
@@ -20,12 +20,13 @@ let
     };
   };
 
-  shell = myHaskellPackages.shellFor {
+  shell = haskell.shellFor {
     packages = p: [
       p.act
     ];
     buildInputs = with pkgs.haskellPackages; [
       cabal-install
+      haskell-language-server
       pkgs.jq
       pkgs.z3
       pkgs.cvc4
@@ -37,12 +38,8 @@ let
       export PATH=${toString ./bin}:$PATH
     '';
   };
-
-  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages.act);
 in
 {
   inherit shell;
-  inherit exe;
-  inherit myHaskellPackages;
-  act = myHaskellPackages.act;
+  exe = haskell.act;
 }
