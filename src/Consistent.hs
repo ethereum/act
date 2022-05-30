dc5eb0a78a771c920d5e5dad1072547f574e9c6d
Mate Soos <mate.soos@ethereum.org>
2022-05-30 17:43:22 +0200

    Fixing up I think

--- src/Consistent.hs
+++ src/Consistent.hs
@@ -54,17 +54,17 @@ type Ctx = Int
 
 -- checkcases -> normalize -> abstractcases
 
-checkcases :: [Exp Bool] -> Error String ()
+checkcases :: [Exp Bool] -> Err ()
 checkcases = undefined
 
 -- To be run like: "checkNoOverlap abstractCases [Exp Bool]". It will then:
 --   Abstract away, while checking that abstractions don't have overlapping variables
 --   Then checks the cases don't overlap.
-checkNoOverlap :: [Err (Exp Bool)] -> IO (Err ())
+checkNoOverlap :: Err [Exp Bool] -> IO (Err ())
 checkNoOverlap x = do
   let config = SMT.SMTConfig {_solver=SMT.Z3, _timeout=100, _debug=False}
   solverInstance <- spawnSolver config
-  let mypairs = pairs (successes x) :: [Exp Bool] -- TODO filtering here!!!
+  let mypairs = pairs x :: [Exp Bool] -- TODO filtering here!!!
   traceM (show mypairs)
   let queries = expToQuery <$> (mypairs) :: [SMT2]
   traceM (show queries)
@@ -107,16 +107,16 @@ start = (0, AbstFunc {setOfVars = Set.empty, expression = Map.empty})
 successes :: [Validation e a] -> [a]
 successes v = [a | Success a <- v]
 
-failures :: [Validation e a] -> [e]
-failures v = [e | Failure e <- v]
+-- failures :: Show e => [Validation e a] -> [String]
+-- failures v = [show e | Failure e <- v]
+failures :: Show e => [Validation e a] -> String
+failures v = concat [show e | Failure e <- v]
 
 -- Checks also DISTINCT cases. Currently, they can overlap
 -- For example: (a<b) can be overlapping with a=c
 -- we can accomplish this via namesFromExp
-abstractCases :: [Exp Bool] -> [Err (Exp Bool)]
--- another way: abstractCases :: [Exp Bool] -> [Err (Exp Bool)]
---              abstractCases a = Success (successes y) where -- which forgets all errors :(
-abstractCases a = (y) where
+abstractCases :: [Exp Bool] -> Err [Exp Bool]
+abstractCases a = Success (successes y) where -- TODO we we lose all Errors here
   (x, y, z) = abstractCasesHelper (a, [], start)
 type MyPair = ([Exp Bool], [Err (Exp Bool)], (Int, AbstFunc))
 abstractCasesHelper :: MyPair -> MyPair
