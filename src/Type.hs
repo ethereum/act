e10a23a651e21d846fe1d4c7ac5d101e25a171f4
zoep <zoe.paraskevopoulou@gmail.com>
2023-03-21 23:15:52 +0200

    Types: remove warnings

--- src/Type.hs
+++ src/Type.hs
@@ -35,6 +35,8 @@ import qualified Syntax.Untyped as U
 import Syntax.Typed
 import Error
 
+import Data.Type.Equality (TestEquality(..))
+
 type Err = Error String
 
 -- | Main typechecking function.
@@ -329,16 +331,15 @@ checkAbiExpr env e (FromAbi typ) = TExp typ <$> checkExpr env typ e
 
 -- | Attempt to typecheck an untyped expression as any possible type.
 typedExp :: Typeable t => Env -> U.Expr -> Err (TypedExp t)
-typedExp env e = fromMaybe (error $ "Internal error: Type.typedExp. Expr: " <> show e)
-                 $ notAtPosn (getPosn e)
-                 [ TExp SInteger <$> checkExpr env SInteger e
-                 , TExp SBoolean <$> checkExpr env SBoolean e
-                 , TExp SByteStr <$> checkExpr env SByteStr e
-                 ]
+typedExp env e = findSuccess (throw (getPosn e, "Cannot find a valid type for expression " <> show e))
+                   [ TExp SInteger <$> checkExpr env SInteger e
+                   , TExp SBoolean <$> checkExpr env SBoolean e
+                   , TExp SByteStr <$> checkExpr env SByteStr e
+                   ]
 
 -- | Check the type of an expression and construct a typed expression
 checkExpr :: forall a t. Typeable t => Env -> SType a -> U.Expr -> Err (Exp a t)
-checkExpr env typ e = case (typ, e) of
+checkExpr env@Env{contract,store,calldata} typ e = case (typ, e) of
   -- Boolean expressions
   (SBoolean, U.ENot    p v1)    -> Neg  p <$> checkExpr env SBoolean v1
   (SBoolean, U.EAnd    p v1 v2) -> And  p <$> checkExpr env SBoolean v1 <*> checkExpr env SBoolean v2
@@ -348,8 +349,8 @@ checkExpr env typ e = case (typ, e) of
   (SBoolean, U.ELEQ    p v1 v2) -> LEQ p  <$> checkExpr env SInteger v1 <*> checkExpr env SInteger v2
   (SBoolean, U.EGEQ    p v1 v2) -> GEQ p  <$> checkExpr env SInteger v1 <*> checkExpr env SInteger v2
   (SBoolean, U.EGT     p v1 v2) -> GT  p  <$> checkExpr env SInteger v1 <*> checkExpr env SInteger v2
-  (SBoolean, U.EEq     p v1 v2) -> polycheck p env Eq v1 v2
-  (SBoolean, U.ENeq    p v1 v2) -> polycheck p env NEq v1 v2
+  (SBoolean, U.EEq     p v1 v2) -> polycheck p Eq v1 v2
+  (SBoolean, U.ENeq    p v1 v2) -> polycheck p NEq v1 v2
   (SBoolean, U.BoolLit p v1)    -> pure $ LitBool p v1
   -- Arithemetic expressions
   (SInteger, U.EAdd    p v1 v2) -> Add p <$> checkExpr env SInteger v1 <*> checkExpr env SInteger v2
@@ -371,46 +372,44 @@ checkExpr env typ e = case (typ, e) of
     Just AByteStr -> pure $ ByEnv p v1
     _            -> throw (p, "Unknown environment variable " <> show v1)
   -- Variable references
-  (_, U.EUTEntry   p name es) -> checkEntry p env Neither name typ es
-  (_, U.EPreEntry  p name es) -> checkEntry p env Pre name typ es
-  (_, U.EPostEntry p name es) -> checkEntry p env Post name typ es
+  (_, U.EUTEntry   p name es) -> checkEntry p Neither name es
+  (_, U.EPreEntry  p name es) -> checkEntry p Pre name es
+  (_, U.EPostEntry p name es) -> checkEntry p Post name es
   -- TODO other error for unimplemented
   _ -> throw (getPosn e,"Type mismatch. Expression " <> show e <> " does not have type " <> show typ)
 
-polycheck :: Typeable t => Pn -> Env -> (forall y. Pn -> SType y -> Exp y t -> Exp y t -> Exp x t) -> U.Expr -> U.Expr -> Err (Exp x t)
-polycheck pn env cons e1 e2 = fromMaybe (error $ "Internal error: Type.polycheck. Expr1: " <> show e1)
-  $ notAtPosn (getPosn e1)
-  [ cons pn SInteger <$> checkExpr env SInteger e1 <*> checkExpr env SInteger e2
-  , cons pn SBoolean <$> checkExpr env SBoolean e1 <*> checkExpr env SBoolean e2
-  , cons pn SByteStr <$> checkExpr env SByteStr e1 <*> checkExpr env SByteStr e2
-  ]
+  where 
+    polycheck :: Pn -> (forall y. Pn -> SType y -> Exp y t -> Exp y t -> Exp x t) -> U.Expr -> U.Expr -> Err (Exp x t)
+    polycheck pn cons e1 e2 = findSuccess (throw (getPosn e1, "Cannot match the type of expression " <> show e1 <> " with expression " <> show e2))
+      [ cons pn SInteger <$> checkExpr env SInteger e1 <*> checkExpr env SInteger e2
+      , cons pn SBoolean <$> checkExpr env SBoolean e1 <*> checkExpr env SBoolean e2
+      , cons pn SByteStr <$> checkExpr env SByteStr e1 <*> checkExpr env SByteStr e2
+      ]
 
+    -- Try to construct a reference to a calldata variable or an item in storage.
+    checkEntry :: forall t0. Typeable t0 => Pn -> Time t0 -> Id -> [U.Expr] -> Err (Exp a t)
+    checkEntry pn timing name es = case (Map.lookup name store, Map.lookup name calldata) of
+      (Nothing, Nothing) -> throw (pn, "Unknown variable " <> name)
+      (Just _, Just _)   -> throw (pn, "Ambiguous variable " <> name)
+      (Nothing, Just (FromAct varType)) ->
+        if isTimed timing then throw (pn, "Calldata var cannot be pre/post")
+        else Var pn typ name <$ checkEq pn typ varType
+      (Just (StorageValue (FromAbi varTyp)), Nothing)      -> makeEntry typ [] <* checkEq pn typ varTyp
+      (Just (StorageMapping ts (FromAbi varTyp)), Nothing) -> makeEntry typ (NonEmpty.toList ts) <* checkEq pn typ varTyp
+      where
+        makeEntry :: SType a -> [AbiType] -> Err (Exp a t)
+        makeEntry entryType ts = checkTime <*> (TEntry pn timing . Item entryType contract name <$> checkIxs pn env es ts)
+  
+        checkTime :: Err (Exp x t0 -> Exp x t)
+        checkTime = case eqT @t @t0 of
+          Just Refl -> pure id
+          Nothing   -> throw (pn, (tail . show $ typeRep @t) <> " variable needed here")
 
+      
 checkEq :: forall a b. Pn -> SType a -> SType b -> Err ()
-checkEq _ SInteger SInteger = pure ()
-checkEq _ SBoolean SBoolean = pure ()
-checkEq _ SByteStr SByteStr = pure ()
-checkEq pn t1 t2 = throw (pn, "Type " <> show t1 <> " should match type " <> show t2)
-
--- XXX why different t t0
--- Try to construct a reference to a calldata variable or an item in storage.
-checkEntry :: forall t a t0. Typeable t => Typeable t0 => Pn -> Env -> Time t0 -> Id -> SType a -> [U.Expr] -> Err (Exp a t)
-checkEntry pn env@Env{contract,store,calldata} timing name typ es = case (Map.lookup name store, Map.lookup name calldata) of
-  (Nothing, Nothing) -> throw (pn, "Unknown variable " <> name)
-  (Just _, Just _)   -> throw (pn, "Ambiguous variable " <> name)
-  (Nothing, Just (FromAct varType)) ->
-    if isTimed timing then throw (pn, "Calldata var cannot be pre/post")
-    else Var pn typ name <$ checkEq pn typ varType
-  (Just (StorageValue (FromAbi varTyp)), Nothing)      -> makeEntry typ [] <* checkEq pn typ varTyp
-  (Just (StorageMapping ts (FromAbi varTyp)), Nothing) -> makeEntry typ (NonEmpty.toList ts) <* checkEq pn typ varTyp
+checkEq p t1 t2 = maybe err (\Refl -> pure ()) $  testEquality t1 t2
   where
-    makeEntry :: SType a -> [AbiType] -> Err (Exp a t)
-    makeEntry entryType ts = checkTime <*> (TEntry pn timing . Item entryType contract name <$> checkIxs pn env es ts)
-
-    checkTime :: Err (Exp x t0 -> Exp x t)
-    checkTime = case eqT @t @t0 of
-      Just Refl -> pure id
-      Nothing   -> throw (pn, (tail . show $ typeRep @t) <> " variable needed here")
+    err = (throw (p, "Type " <> show t1 <> " should match type " <> show t2)) 
 
 
 -- | Checks that there are as many expressions as expected by the types,
