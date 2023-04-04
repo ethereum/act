d94ca3b1384ea1903a8e09aec3e521d6b45059d8
zoep <zoe.paraskevopoulou@gmail.com>
2023-04-04 14:10:44 +0300

    Test: regenerate test outputs

--- tests/frontend/pass/array/array.act.parsed.hs
+++ tests/frontend/pass/array/array.act.parsed.hs
@@ -1,1 +1,1 @@
-[Transition (AlexPn 0 1 1) "f" "A" f(address[2] xs) [] (Direct (Post [] [] (Just (EUTEntry (AlexPn 53 4 9) "xs" [IntLit (AlexPn 56 4 12) 1])))) []]
+[Transition (AlexPn 0 1 1) "f" "A" f(address[2] xs) [] (Direct (Post [] [] (Just (EUTEntry (EMapping (AlexPn 55 4 11) (EVar (AlexPn 53 4 9) "xs") [IntLit (AlexPn 56 4 12) 1]))))) []]
