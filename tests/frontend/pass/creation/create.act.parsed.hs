15067a32252203e379c1c1e0a97ce9b86dba51dc
zoep <zoe.paraskevopoulou@gmail.com>
2023-04-05 16:59:17 +0300

    Test: regenerate test outputs

--- tests/frontend/pass/creation/create.act.parsed.hs
+++ tests/frontend/pass/creation/create.act.parsed.hs
@@ -1,1 +1,1 @@
-[Definition (AlexPn 439 9 16) "Modest" constructor() [] (Creates [AssignVal (StorageVar (AlexPn 661 16 10) (StorageValue (PrimitiveType uint256)) "x") (IntLit (AlexPn 666 16 15) 1),AssignVal (StorageVar (AlexPn 680 17 13) (StorageValue (PrimitiveType address)) "y") (EnvExp (AlexPn 685 17 18) Caller)]) [] [] []]
+[Definition (AlexPn 439 9 16) "Modest" constructor() [] (Creates [AssignVal (StorageVar (AlexPn 661 16 10) uint256 "x") (IntLit (AlexPn 666 16 15) 1),AssignVal (StorageVar (AlexPn 680 17 13) address "y") (EnvExp (AlexPn 685 17 18) Caller)]) [] [] []]
