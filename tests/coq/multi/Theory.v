78ea353f5044a2c384ab019f5dec4f27aeb8b144
zoep <zoe.paraskevopoulou@gmail.com>
2023-05-11 11:29:11 +0300

    tests: Coq test for multiple contracts

---
+++ tests/coq/multi/Theory.v
@@ -0,0 +1,28 @@
+Require Import Multi.Multi.
+Require Import Coq.ZArith.ZArith.
+Require Import ActLib.ActLib.
+
+
+Import C.
+
+       
+Theorem reachable_value_f S0 S:
+  reachable S0 S ->
+  forall x, A.f (B.a (b S)) x = 0 \/ A.f (B.a (b S)) x = 2.
+Proof.
+  intros HR x. induction HR.
+  - simpl; eauto.
+
+  - simpl. destruct (x =? i).
+    + eauto.
+    + eauto. 
+
+  - eauto.
+Qed.
+
+Theorem reachable_value_x S0 S:
+  reachable S0 S ->
+  w S = 0 \/ w S = 1. 
+Proof.
+  intros HR. induction HR; simpl; eauto. 
+Qed.
