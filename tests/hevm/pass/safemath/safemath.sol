2cf55d277fba0c882860e1125bfd4e7251eb3981
zoep <zoe.paraskevopoulou@gmail.com>
2023-05-26 15:12:14 +0200

    tests: add more behaviours to safemath

--- tests/hevm/pass/safemath/safemath.sol
+++ tests/hevm/pass/safemath/safemath.sol
@@ -8,4 +8,9 @@ contract SafeAdd {
     function mul(uint x, uint y) public pure returns (uint z) {
         require((z = x * y) >= x);
     }
+
+    function double(uint x) public pure returns (uint z) {
+	require((z = 2 * x) >= x);
+    }
+
 }
