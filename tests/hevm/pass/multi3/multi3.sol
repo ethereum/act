914e7f35c80825e865128ea589860cbd8c4dc54f
zoep <zoe.paraskevopoulou@gmail.com>
2023-10-19 15:32:20 +0300

    test: multi test

--- tests/hevm/pass/multi3/multi3.sol
+++ tests/hevm/pass/multi3/multi3.sol
@@ -4,15 +4,28 @@ contract A {
     constructor (uint z)  {
 	x = z + 1;
     }
+
+    function add_x(uint y) public {
+	x = x + y;
+    }
 }
 
 contract B {
     uint z;
+    A a;
 
     constructor (uint i)  {
 	z = i + 42;
+	a = new A(i);
     }
 
+    function incr_z() public {
+	z = z + 1;
+    }
+
+    function a_add_x(uint y) public {
+	a.add_x(y);
+    }
 }
 
 contract C {
@@ -26,4 +39,11 @@ contract C {
 	a = new A(u);
 	b = new B(u);
     }
+
+    function remote(uint z) public returns (uint) {
+	b.incr_z();
+	b.a_add_x(z);
+	return z;
+    }
 }
+
