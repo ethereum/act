790795e40c97e08840cddd49aa6e63f0f2bfbfeb
zoep <zoe.paraskevopoulou@gmail.com>
2023-12-06 18:51:06 +0200

    add files

---
+++ tests/hevm/multi4/multi4.sol
@@ -0,0 +1,45 @@
+contract A {
+    uint public x;
+
+    constructor (uint z)  {
+	x = z + 1;
+    }
+
+    function add_x(uint y) public {
+	x = x + y;
+    }
+}
+
+contract B {
+    uint z;
+    A a;
+
+    constructor (uint i)  {
+	z = i + 42;
+	a = new A(i);
+    }
+
+    function foo() public {
+	a = new A(42);
+    }
+}
+
+/* contract C { */
+
+/*     uint y; */
+/*     A a; */
+/*     B b; */
+
+/*     constructor(uint u) { */
+/* 	y = 0; */
+/* 	a = new A(u); */
+/* 	b = new B(u); */
+/*     } */
+
+/*     function remote(uint z) public returns (uint) { */
+/* 	b.incr_z(); */
+/* 	b.a_add_x(z); */
+/* 	return z; */
+/*     } */
+/* } */
+
