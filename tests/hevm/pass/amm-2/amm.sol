0ac36b486492b82d4193514611aab85a465ae6c1
zoep <zoe.paraskevopoulou@gmail.com>
2024-07-15 15:48:10 +0300

    WIP checking aliasing

---
+++ tests/hevm/pass/amm-2/amm.sol
@@ -0,0 +1,51 @@
+pragma solidity >=0.8.0;
+
+contract Token {
+    mapping (address => uint) public balanceOf;
+
+    constructor(uint _totalSupply) {
+        balanceOf[msg.sender] = _totalSupply;
+    }
+
+
+    function transferFrom(uint256 value, address from, address to) public returns (uint) {
+	balanceOf[from] = balanceOf[from] - value;
+        balanceOf[to] = balanceOf[to] + value;
+        return 1;
+    }
+
+}
+
+
+contract Amm {
+
+    Token token0;
+    Token token1;
+
+    constructor(address t0, address t1) {
+	require (t0 != t1);
+        token0 = Token(t0);
+	token1 = Token(t1);
+    }
+
+    function swap0(uint256 amt) public returns (uint) {
+	uint256 reserve0 = token0.balanceOf(address(this));
+	uint256 reserve1 = token1.balanceOf(address(this));
+
+	token0.transferFrom(amt, msg.sender, address(this));
+	token1.transferFrom((reserve1*amt)/(reserve0+amt), address(this), msg.sender);
+
+	return 1;
+    }
+
+    function swap1(uint256 amt) public returns (uint) {
+	uint256 reserve0 = token0.balanceOf(address(this));
+	uint256 reserve1 = token1.balanceOf(address(this));
+
+	token1.transferFrom(amt, msg.sender, address(this));
+	token0.transferFrom((reserve0*amt)/(reserve1+amt), address(this), msg.sender);
+
+	return 1;
+    }
+
+}
--- tests/hevm/pass/cast/cast.sol
+++ tests/hevm/pass/cast/cast.sol
@@ -24,6 +24,7 @@ contract B {
     A a2;
 
     constructor(address x, address y) {
+	require(x!=y);
 	a1 = A(x);
 	a2 = A(y);
     }
