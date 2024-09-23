9e60317edd6711cbeabff3e89eed4835861a379b
zoep <zoe.paraskevopoulou@gmail.com>
2024-09-23 12:41:56 +0300

    regenerate parse tests

--- tests/frontend/pass/smoke/smoke.act.parsed.hs
+++ tests/frontend/pass/smoke/smoke.act.parsed.hs
@@ -1,1 +1,1 @@
-Main [Contract (Definition (AlexPn 0 0 0) "A" constructor() [] (Creates []) [] []) [Transition (AlexPn 0 1 1) "f" "A" f(uint256 x) [] (Direct (Post [] (Just (IntLit (AlexPn 46 4 9) 1)))) []]]
+Main [Contract (Definition (AlexPn 0 0 0) "A" constructor() [] [] (Creates []) [] []) [Transition (AlexPn 0 1 1) "f" "A" f(uint256 x) [] [] (Direct (Post [] (Just (IntLit (AlexPn 46 4 9) 1)))) []]]
