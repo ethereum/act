78a9b14af9ec437faf054f2eaa947ee37503444f
zoep <zoe.paraskevopoulou@gmail.com>
2025-06-30 18:09:34 +0300

    Test: remove hard tabs in fix indentation

--- tests/hevm/pass/amm/amm.sol
+++ tests/hevm/pass/amm/amm.sol
@@ -9,7 +9,7 @@ contract Token {
 
 
     function transferFrom(uint256 value, address from, address to) public returns (uint) {
-	balanceOf[from] = balanceOf[from] - value;
+        balanceOf[from] = balanceOf[from] - value;
         balanceOf[to] = balanceOf[to] + value;
         return 1;
     }
@@ -24,27 +24,27 @@ contract Amm {
 
     constructor(uint256 amt1, uint256 amt2) {
         token0 = new Token(amt1);
-	token1 = new Token(amt2);
+        token1 = new Token(amt2);
     }
 
     function swap0(uint256 amt) public returns (uint) {
-	uint256 reserve0 = token0.balanceOf(address(this));
-	uint256 reserve1 = token1.balanceOf(address(this));
+        uint256 reserve0 = token0.balanceOf(address(this));
+        uint256 reserve1 = token1.balanceOf(address(this));
 
-	token0.transferFrom(amt, msg.sender, address(this));
-	token1.transferFrom((reserve1*amt)/(reserve0+amt), address(this), msg.sender);
+        token0.transferFrom(amt, msg.sender, address(this));
+        token1.transferFrom((reserve1*amt)/(reserve0+amt), address(this), msg.sender);
 
-	return 1;
+        return 1;
     }
 
     function swap1(uint256 amt) public returns (uint) {
-	uint256 reserve0 = token0.balanceOf(address(this));
-	uint256 reserve1 = token1.balanceOf(address(this));
+        uint256 reserve0 = token0.balanceOf(address(this));
+        uint256 reserve1 = token1.balanceOf(address(this));
 
-	token1.transferFrom(amt, msg.sender, address(this));
-	token0.transferFrom((reserve0*amt)/(reserve1+amt), address(this), msg.sender);
+        token1.transferFrom(amt, msg.sender, address(this));
+        token0.transferFrom((reserve0*amt)/(reserve1+amt), address(this), msg.sender);
 
-	return 1;
+        return 1;
     }
 
 }
