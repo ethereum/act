da87b0112869fa8f4fef5546d20bd612c09063b6
zoep <zoe.paraskevopoulou@gmail.com>
2025-07-02 14:17:58 +0300

    Test: fix failing proof

--- tests/coq/transitions/Theory.v
+++ tests/coq/transitions/Theory.v
@@ -9,22 +9,6 @@ Import StateMachine.
 
 Theorem invariant : forall BASE s, reachable BASE s -> (x s) >= 0 /\ (x s) <= 2.
 Proof.
-  intros. induction H. {
-    simpl. split.
-    - intros contra. discriminate.
-    - intros contra. discriminate.
-  } {
-    simpl. split.
-    - intros contra. discriminate.
-    - intros contra. discriminate.
-  } {
-    simpl. split.
-    - intros contra. discriminate.
-    - intros contra. discriminate.
-  } {
-    simpl. split.
-    - intros contra. discriminate.
-    - intros contra. discriminate.
-  }
-Qed. Check invariant.
+  intros. induction H; simpl; split; easy.
+Qed.
 
